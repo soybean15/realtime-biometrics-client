1d835f28f9f3711db81f159166e5950b17353529
soybean15 <marlonpadilla1593@gmail.com>
2023-08-24 11:59:08 +0800

    creating reusable Data table

---
+++ src/views/admin/components/UserTable.vue
@@ -0,0 +1,66 @@
+<template>
+  <DataTable :title="'Users'" :rows="rows" :columns="columns"/>
+  {{ users }}
+</template>
+
+<script>
+import DataTable from '@/components/DataTable.vue'
+import { useUserStore } from '@/store/user'
+import { storeToRefs } from 'pinia'
+import { onMounted } from 'vue'
+
+
+
+
+const columns = [
+    {
+      name: 'name',
+      required: true,
+      label: 'Name',
+      align: 'left',
+      field: row => row.name,
+      format: val => `${val}`,
+      sortable: true
+    },
+    
+  ]
+
+  const rows = [
+   { "id": 1, "name": "Miguel Gorczany III", "email": "mparker@example.org", "email_verified_at": "2023-08-23T10:46:34.000000Z", "two_factor_secret": null, "two_factor_recovery_codes": null, "two_factor_confirmed_at": null, "created_at": "2023-08-23T10:46:34.000000Z", "updated_at": "2023-08-23T10:46:34.000000Z", "roles": [], "admin": false }, 
+  ]
+
+  
+
+
+export default {
+    components:{
+        DataTable
+    },
+    setup(){
+
+        const store = useUserStore()
+
+        const {users} = storeToRefs(store)
+
+        
+
+        onMounted(()=>{
+            store.getUsers()
+        })
+
+
+         
+
+        return{
+            users,
+            columns,
+            rows
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
