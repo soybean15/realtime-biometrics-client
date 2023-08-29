0e6f45eb91ce17c4054817c077b3dd61a28bcef1
soybean15 <marlonpadilla1593@gmail.com>
2023-08-29 20:31:06 +0800

    added job detail component

---
+++ src/views/admin/components/modals/children/JobDetailForm.vue
@@ -0,0 +1,30 @@
+<template>
+  {{ departments }}
+  {{ positions }}
+</template>
+
+<script>
+import {useDepartmentStore} from '@/store/department'
+import {usePositionStore} from '@/store/position'
+import { storeToRefs } from 'pinia'
+export default {
+
+    setup(){
+        const department = useDepartmentStore()
+        const position = usePositionStore()
+
+        const {departments} = storeToRefs(department)
+        const {positions} = storeToRefs(position) 
+
+        return {
+            positions,
+            departments
+        }
+
+    }
+}
+</script>
+
+<style>
+
+</style>
