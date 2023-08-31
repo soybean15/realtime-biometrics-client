5bb16a5c495006f76c5454ceb1b80a5d225027ac
soybean15 <marlonpadilla1593@gmail.com>
2023-08-31 21:42:21 +0800

    created employee profile

--- src/views/admin/components/EmployeeDetails.vue
+++ src/views/admin/components/EmployeeDetails.vue
@@ -6,7 +6,7 @@
     </div>
 
     <div>
-      <span class="bg-surface">{{selectedEmployee.created_at}}</span>
+      <span class="bg-surface">{{ selectedEmployee.created_at }}</span>
       <q-btn outline label="delete" color="red"></q-btn>
     </div>
   </div>
@@ -26,30 +26,74 @@
     </q-tabs>
   </div>
 
-  <div class="row h-40 m-2  shadow-sm rounded-lg">
-    <div class="col-6 p-2 pr-1 ">
+  <div class="row h-40 m-2 shadow-sm rounded-lg">
+    <div class="col-6 p-2 pr-1">
+      <div class="p-2 py-5 bg-surface rounded-lg shadow-lg">
+        <div class="column">
+          <div class="row">
+            <div class="col-6 column items-center justify-center">
+              <q-avatar size="120px" rounded>
+                <img :src="selectedEmployee.image" />
+              </q-avatar>
+              <span class="p-2 pb-0 font-semibold">{{
+                selectedEmployee.full_name
+              }}</span>
+              <span
+                class="bg-gray-800 text-white text-sm font-bold p-1 rounded-md"
+                >{{ `ID: ${selectedEmployee.employee_id}` }}</span
+              >
 
-    <div class="p-2 bg-surface rounded-lg">
+              <div class="self-start p-3 m-2">
+                <div class="font-secondary text-sm">Email</div>
+                <div class="text-md font-bold font-primary">{{ selectedEmployee.email }}</div>
+                <div class="font-secondary text-sm ">
+                  Contact Number
+                </div>
+                <div class="text-md  font-bold font-primary">{{ selectedEmployee.contact_number }}</div>
+                <div class="font-secondary text-sm">Birth Date</div>
+                <div class="text-md  font-bold font-primary">{{ selectedEmployee.birthdate }}</div>
+                <div class="font-secondary text-sm">Gender</div>
+                <div class="text-md  font-bold font-primary">{{ selectedEmployee.gender }}</div>
+              </div>
+            </div>
 
-      <div class="column">
-        <div class="row">
-          <div class="col-8 column items-center justify-center row">
-            <q-avatar size="120px" rounded>
-              <img :src="selectedEmployee.image" />
-            </q-avatar>
-            <span>{{ selectedEmployee.full_name }}</span>
+            <div class="col-6  column">
+              <div class="mb-5">
+                <div class="font-secondary text-sm">Position</div>
+                <div class="column  font-bold font-primary">
+                  <div
+                    v-for="position in selectedEmployee.positions"
+                    :key="position.id"
+                  >
+                    {{ position.name }}
+                  </div>
+                </div>
+              </div>
+
+              <div class="mb-5">
+                <div class="font-secondary text-sm">Deparment/s</div>
+                <div class="column  font-bold font-primary">
+                  <div
+                    v-for="department in selectedEmployee.departments"
+                    :key="department.id"
+                  >
+                    {{ department.name }}
+                  </div>
+                </div>
+              </div>
+              <div class="mb-5">
+                <div class="font-secondary text-sm">Address</div>
+                <div class="  font-bold font-primary">
+                  {{ selectedEmployee.address }}
+                </div>
+              </div>
+            </div>
           </div>
-          <div class="col-4"></div>
         </div>
       </div>
-
     </div>
-  
-    </div>
-    <div class="col-6 p-2 pl-1 ">
-      <div class="col-6 bg-surface rounded-lg h-40">
-
-      </div>
+    <div class="col-6 p-2 pl-1">
+      <div class="col-6 bg-surface rounded-lg h-40"></div>
     </div>
   </div>
 </template>
@@ -58,18 +102,16 @@
 import { useEmployeeStore } from "@/store/employee";
 import { storeToRefs } from "pinia";
 import router from "@/router";
-import { ref } from 'vue';
+import { ref } from "vue";
 
 export default {
   setup() {
     const _employee = useEmployeeStore();
     const { selectedEmployee } = storeToRefs(_employee);
 
-
-    
     return {
       selectedEmployee,
-      tab: ref('profile'),
+      tab: ref("profile"),
       back: () => {
         router.go(-1);
       },
