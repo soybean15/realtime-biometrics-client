270441148db2ac725c743d1fcf2f3393599eaa54
soybean15 <marlonpadilla1593@gmail.com>
2023-09-03 20:29:48 +0800

    added select and date input on dynamic input

--- src/views/admin/components/EmployeeDetails.vue
+++ src/views/admin/components/EmployeeDetails.vue
@@ -1,210 +1,241 @@
 <template>
   <div v-if="selectedEmployee">
-
     <div class="row p-2 items-center justify-between">
-    <div class="row items-center cursor-pointer">
-      <q-icon @click="back" size="1.5rem" name="arrow_back"></q-icon>
-      <span class="text-md"> Employee Details</span>
-    </div>
+      <div class="row items-center cursor-pointer">
+        <q-icon @click="back" size="1.5rem" name="arrow_back"></q-icon>
+        <span class="text-md"> Employee Details</span>
+      </div>
 
-    <div class="row items-center px-2">
-      <span class="bg-surface p-1 mx-2 rounded-md shadow-sm font-secondary">{{
-        `Created at: ${formatTime(selectedEmployee.created_at)}`
-      }}</span>
+      <div class="row items-center px-2">
+        <span class="bg-surface p-1 mx-2 rounded-md shadow-sm font-secondary">{{
+          `Created at: ${formatTime(selectedEmployee.created_at)}`
+        }}</span>
 
-      <ConfirmDialog :pos="'top'">
-        <template v-slot:open-button="{ open }">
-          <q-btn @click="open" dense label="delete" color="red"></q-btn>
-        </template>
+        <ConfirmDialog :pos="'top'">
+          <template v-slot:open-button="{ open }">
+            <q-btn @click="open" dense label="delete" color="red"></q-btn>
+          </template>
 
-        <template v-slot:title>Delete Employee</template>
+          <template v-slot:title>Delete Employee</template>
 
-        <template v-slot:message>{{
-          `Delete ${selectedEmployee.full_name}?`
-        }}</template>
+          <template v-slot:message>{{
+            `Delete ${selectedEmployee.full_name}?`
+          }}</template>
 
-        <template v-slot:buttons="{ close }">
-          <div class="m-2">
-            <q-btn
-              outline
-              @click="onDelete"
-              :loading="loading"
-              size=".8rem"
-              class="mx-1"
-              dense
-              label="Confirm"
-              color="red"
-            ></q-btn>
-            <q-btn
-              @click="close"
-              outline
-              size=".8rem"
-              class="mx-1"
-              dense
-              label="Cancel"
-              color="primary"
-            ></q-btn>
-          </div>
-        </template>
-      </ConfirmDialog>
-    </div>
-  </div>
-
-  <div class="row">
-    <q-tabs
-      v-model="tab"
-      dense
-      class="text-grey"
-      active-color="primary"
-      indicator-color="primary"
-      align="justify"
-      narrow-indicator
-    >
-      <q-tab name="profile" label="Profile" />
-      <q-tab name="attendance" label="Attendance" />
-    </q-tabs>
-  </div>
-
-  <div class="row h-40 m-2 shadow-sm rounded-lg">
-    <div class="col-6 p-2 pr-1">
-      <div class="p-2 py-5 bg-surface rounded-lg shadow-lg">
-        <div class="column">
-          <div class="row">
-            <div class="col-6 column items-center justify-center">
-              <q-avatar size="120px" rounded>
-                <img :src="selectedEmployee.image" />
-              </q-avatar>
-              <span class="p-2 pb-0 font-semibold">{{
-                selectedEmployee.full_name
-              }}</span>
-              <span
-                class="bg-gray-800 text-white text-sm font-bold p-1 rounded-md"
-                >{{ `ID: ${selectedEmployee.employee_id}` }}</span
-              >
-
-              <div class="self-start p-3 m-2 w-full">
-                <LabelInput
-                  :title="'Email'"
-                  :titleClass="'font-secondary font-bold'"
-                  :value="selectedEmployee.email"
-                  :attribute="'email'"
-                  :valueClass="'text-md font-bold font-primary'"
-                  @update="onUpdate"
-                />
-  
-
-                <LabelInput
-                  :title="'Contact Number'"
-                  :titleClass="'font-secondary font-bold'"
-                  :value="selectedEmployee.contact_number"
-                  :valueClass="'text-md font-bold font-primary'"
-                  :attribute="'contact_number'"
-                  @update="onUpdate"
-                />
-
-
-                <LabelInput
-                  :title="'Birth Date'"
-                  :titleClass="'font-secondary font-bold'"
-                  :value="selectedEmployee.birthdate"
-                  :type="'date'"
-                  :valueClass="'text-md font-bold font-primary'"
-                  :attribute="'birthdate'"
-                  @update="onUpdate"
-                />
-
-
-                <LabelInput
-                  :title="'Gender'"
-                  :titleClass="'font-secondary font-bold'"
-                  :value="selectedEmployee.gender"
-                  :type="'select'"
-                  :data="['Male', 'Female']"
-                  :valueClass="'text-md font-bold font-primary'"
-                  :attribute="'gender'"
-                  @update="onUpdate"
-                />
-
-
-              </div>
+          <template v-slot:buttons="{ close }">
+            <div class="m-2">
+              <q-btn
+                outline
+                @click="onDelete"
+                :loading="loading"
+                size=".8rem"
+                class="mx-1"
+                dense
+                label="Confirm"
+                color="red"
+              ></q-btn>
+              <q-btn
+                @click="close"
+                outline
+                size=".8rem"
+                class="mx-1"
+                dense
+                label="Cancel"
+                color="primary"
+              ></q-btn>
             </div>
+          </template>
+        </ConfirmDialog>
+      </div>
+    </div>
 
-            <div class="col-6 column">
-              <div class="mb-5">
-                <div class="font-secondary text-sm">Position</div>
-                <div class="column font-bold font-primary">
-                  <div
-                    v-for="position in selectedEmployee.positions"
-                    :key="position.id"
-                  >
-                    {{ position.name }}
+    <div class="row">
+      <q-tabs
+        v-model="tab"
+        dense
+        class="text-grey"
+        active-color="primary"
+        indicator-color="primary"
+        align="justify"
+        narrow-indicator
+      >
+        <q-tab name="profile" label="Profile" />
+        <q-tab name="attendance" label="Attendance" />
+      </q-tabs>
+    </div>
+
+    <div class="row h-40 m-2 shadow-sm rounded-lg">
+      <div class="col-6 p-2 pr-1">
+        <div class="p-2 py-5 bg-surface rounded-lg shadow-lg">
+          <div class="column">
+            <div class="row">
+              <div class="col-6 column items-center justify-center">
+                <q-avatar size="120px" rounded>
+                  <img :src="selectedEmployee.image" />
+                </q-avatar>
+                <span class="p-2 pb-0 font-semibold">{{
+                  selectedEmployee.full_name
+                }}</span>
+                <span
+                  class="bg-gray-800 text-white text-sm font-bold p-1 rounded-md"
+                  >{{ `ID: ${selectedEmployee.employee_id}` }}</span
+                >
+
+                <div class="self-start p-3 m-2 w-full">
+                  <div class="row">
+                    <div class="col-6">
+                      <LabelInput
+                        :title="'Firstname'"
+                        :titleClass="'font-secondary font-bold'"
+                        :value="selectedEmployee.firstname"
+                        :attribute="'firstname'"
+                        :valueClass="'text-md font-bold font-primary'"
+                        @update="onUpdate"
+                      />
+
+                    </div>
+                    <div class="col-5">
+                      <LabelInput
+                        :title="'Lastname'"
+                        :titleClass="'font-secondary font-bold'"
+                        :value="selectedEmployee.lastname"
+                        :attribute="'lastname'"
+                        :valueClass="'text-md font-bold font-primary'"
+                        @update="onUpdate"
+                      />
+                      
+                    </div>
+        
                   </div>
+
+                  <div class="row">
+                    <div class="col-6">
+                      <LabelInput
+                        :title="'Middlename'"
+                        :titleClass="'font-secondary font-bold'"
+                        :value="selectedEmployee.middlename"
+                        :attribute="'middlename'"
+                        :valueClass="'text-md font-bold font-primary'"
+                        @update="onUpdate"
+                      />
+
+                    </div>
+                    
+  
+        
+                  </div>
+                  <LabelInput
+                    :title="'Email'"
+                    :titleClass="'font-secondary font-bold'"
+                    :value="selectedEmployee.email"
+                    :attribute="'email'"
+                    :valueClass="'text-md font-bold font-primary'"
+                    @update="onUpdate"
+                  />
+
+                  <LabelInput
+                    :title="'Contact Number'"
+                    :titleClass="'font-secondary font-bold'"
+                    :value="selectedEmployee.contact_number"
+                    :valueClass="'text-md font-bold font-primary'"
+                    :attribute="'contact_number'"
+                    @update="onUpdate"
+                  />
+
+                  <LabelInput
+                    :title="'Birth Date'"
+                    :titleClass="'font-secondary font-bold'"
+                    :value="selectedEmployee.birthdate"
+                    :type="'date'"
+                    :valueClass="'text-md font-bold font-primary'"
+                    :attribute="'birthdate'"
+                    @update="onUpdate"
+                  />
+
+                  <LabelInput
+                    :title="'Gender'"
+                    :titleClass="'font-secondary font-bold'"
+                    :value="selectedEmployee.gender"
+                    :type="'select'"
+                    :data="['Male', 'Female']"
+                    :valueClass="'text-md font-bold font-primary'"
+                    :attribute="'gender'"
+                    @update="onUpdate"
+                  />
                 </div>
               </div>
-            
 
-
-              <div class="mb-5">
-                <div class="font-secondary text-sm">Deparment/s</div>
-                <div class="column font-bold font-primary">
-                  <div
-                    v-for="department in selectedEmployee.departments"
-                    :key="department.id"
-                  >
-                    {{ department.name }}
+              <div class="col-6 column">
+                <div class="mb-5">
+                  <div class="font-secondary text-sm">Position</div>
+                  <div class="column font-bold font-primary">
+                    <div
+                      v-for="position in selectedEmployee.positions"
+                      :key="position.id"
+                    >
+                      {{ position.name }}
+                    </div>
                   </div>
                 </div>
-              </div>
-              
 
-              <LabelInput
+                <div class="mb-5">
+                  <div class="font-secondary text-sm">Deparment/s</div>
+                  <div class="column font-bold font-primary">
+                    <div
+                      v-for="department in selectedEmployee.departments"
+                      :key="department.id"
+                    >
+                      {{ department.name }}
+                    </div>
+                  </div>
+                </div>
+
+                <LabelInput
                   :title="'Address'"
                   :titleClass="'font-secondary font-bold'"
                   :value="selectedEmployee.address"
-                
                   :valueClass="'text-md font-bold font-primary'"
                   :attribute="'address'"
                   @update="onUpdate"
                 />
 
-              <div class="mb-5">
-                <div class="font-secondary text-sm">Date Created:</div>
-                <div class="font-bold font-primary">
-                  {{ formatTime(selectedEmployee.created_at) }}
+                <div class="mb-5">
+                  <div class="font-secondary text-sm">Date Created:</div>
+                  <div class="font-bold font-primary">
+                    {{ formatTime(selectedEmployee.created_at) }}
+                  </div>
                 </div>
-              </div>
-              <div class="mb-5">
-                <div class="font-secondary text-sm">Last Updated:</div>
-                <div class="font-bold font-primary">
-                  {{ formatTime(selectedEmployee.updated_at) }}
+                <div class="mb-5">
+                  <div class="font-secondary text-sm">Last Updated:</div>
+                  <div class="font-bold font-primary">
+                    {{ formatTime(selectedEmployee.updated_at) }}
+                  </div>
                 </div>
-              </div>
 
-              <div class="mb-5">
-                <div class="font-secondary text-sm">Added By:</div>
-                <div class="font-bold font-primary">
-                  {{ selectedEmployee.user.name }}
+                <div class="mb-5">
+                  <div class="font-secondary text-sm">Added By:</div>
+                  <div class="font-bold font-primary">
+                    {{ selectedEmployee.user.name }}
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    <div class="col-6 p-2 pl-1">
-      <div class="col-6 bg-surface rounded-lg h-40"></div>
+      <div class="col-6 p-2 pl-1">
+        <div class="col-6 bg-surface rounded-lg h-40"></div>
+      </div>
     </div>
   </div>
-
-  </div>
-  </template>
+</template>
 
 <script>
 import { useEmployeeStore } from "@/store/employee";
 import { storeToRefs } from "pinia";
 import router from "@/router";
 import ConfirmDialog from "@/components/ConfirmDialog.vue";
-import {  onMounted, ref } from "vue";
+import { onMounted, ref } from "vue";
 import DateTimeFormatter from "@/composables/DateTimeFormat";
 import LabelInput from "@/components/LabelInput.vue";
 import { usePositionStore } from "@/store/position";
@@ -215,20 +246,20 @@ export default {
   setup() {
     const _employee = useEmployeeStore();
     const { selectedEmployee } = storeToRefs(_employee);
-    const _position = usePositionStore()
-    const {positions } = storeToRefs(_position)
+    const _position = usePositionStore();
+    const { positions } = storeToRefs(_position);
 
     const loading = ref(false);
 
-    onMounted(()=>{
-      if(!selectedEmployee.value){
-        console.log("null")
-        const id = router.currentRoute.value.params.id
-        _employee.get(id)
+    onMounted(() => {
+      if (!selectedEmployee.value) {
+        console.log("null");
+        const id = router.currentRoute.value.params.id;
+        _employee.get(id);
       }
-    })
-    
-    console.log(selectedEmployee.value)
+    });
+
+    console.log(selectedEmployee.value);
 
     return {
       selectedEmployee,
@@ -251,10 +282,9 @@ export default {
         return new DateTimeFormatter(timestamp).format("MMM D, YYYY");
         //  return moment(timestamp).format("MMM D, YYYY");
       },
-      onUpdate:(attribute,val)=>{
-        _employee.update(attribute,val)
-     
-      }
+      onUpdate: (attribute, val) => {
+        _employee.update(attribute, val);
+      },
     };
   },
 };
