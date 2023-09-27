51167479e20ed5a1567a2ce5c2ae595aa3deda15
soybean15 <marlonpadilla1593@gmail.com>
2023-09-27 22:21:22 +0800

    fixing qcalendar

--- src/components/QCalendar/WeeklyAgenda.vue
+++ src/components/QCalendar/WeeklyAgenda.vue
@@ -8,7 +8,7 @@
 
     <div class="row justify-center">
       <div style="display: flex; max-width: 800px; width: 100%; height: 400px;">
-        <q-calendar
+        <q-calendar-day
           ref="calendar"
           v-model="selectedDate"
           view="week"
@@ -22,23 +22,71 @@
           @click-interval="onClickInterval"
           @click-head-intervals="onClickHeadIntervals"
           @click-head-day="onClickHeadDay"
-          
         >
-        <template #day-body="{ }">
-          test
-       
-      </template>
+        <template #head-day-event="{ scope: { timestamp } }">
+            <div style="display: flex; justify-content: center; flex-wrap: wrap; padding: 2px;">
+              <template
+                v-for="event in eventsMap[timestamp.date]"
+                :key="event.id"
+              >
+                <q-badge
+                  v-if="!event.time"
+                  :class="badgeClasses(event, 'header')"
+                  :style="badgeStyles(event, 'header')"
+                  style="width: 100%; cursor: pointer; height: 12px; font-size: 10px; margin: 1px;"
+                >
+                  <div class="title q-calendar__ellipsis">
+                    {{ event.title }}
+                    <q-tooltip>{{ event.details }}</q-tooltip>
+                  </div>
+                </q-badge>
+                <q-badge
+                  v-else
+                  :class="badgeClasses(event, 'header')"
+                  :style="badgeStyles(event, 'header')"
+                  style="margin: 1px; width: 10px; max-width: 10px; height: 10px; max-height: 10px; cursor: pointer"
+                  @click="scrollToEvent(event)"
+                >
+                  <q-tooltip>{{ event.time + ' - ' + event.details }}</q-tooltip>
+                </q-badge>
+              </template>
+            </div>
+          </template>
 
+          <template #day-body="{ scope: { timestamp, timeStartPos, timeDurationHeight } }">
+            <template
+              v-for="event in getEvents(timestamp.date)"
+              :key="event.id"
+            >
+              <div
+                v-if="event.time !== undefined"
+                class="my-event"
+                :class="badgeClasses(event, 'body')"
+                :style="badgeStyles(event, 'body', timeStartPos, timeDurationHeight)"
+              >
+                <div class="title q-calendar__ellipsis">
+                  {{ event.title }}
+                  <q-tooltip>{{ event.time + ' - ' + event.details }}</q-tooltip>
+                </div>
+              </div>
+            </template>
+          </template>
       
-        
-      </q-calendar>
+      </q-calendar-day>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { QCalendar ,today} from '@quasar/quasar-ui-qcalendar'
+import {  
+  QCalendarDay,
+  addToDate,
+  parseTimestamp,
+  isBetweenDates,
+  today,
+  parsed,
+  parseTime} from '@quasar/quasar-ui-qcalendar/src/index.js'
 import '@quasar/quasar-ui-qcalendar/src/QCalendarVariables.sass'
 import '@quasar/quasar-ui-qcalendar/src/QCalendarTransitions.sass'
 import '@quasar/quasar-ui-qcalendar/src/QCalendarDay.sass'
@@ -46,15 +94,11 @@ import { useEmployeeStore } from '@/store/employee'
 import { defineComponent } from 'vue'
 import NavigationBar from './NavigationBar.vue'
 
-
-
-
-
 export default defineComponent({
   name: 'WeekDark',
   components: {
     NavigationBar,
-    QCalendar
+    QCalendarDay
   },
   data () {
     return {
@@ -63,7 +107,53 @@ export default defineComponent({
     }
   },
 
+  computed: {
+    // convert the events into a map of lists keyed by date
+    eventsMap () {
+      const map = {}
+      // this.events.forEach(event => (map[ event.date ] = map[ event.date ] || []).push(event))
+
+      console.log(this.employeeStore.employeeAttendance)
+      this.employeeStore.employeeAttendance.forEach(event => {
+        if (!map[ event.date ]) {
+          map[ event.date ] = []
+        }
+        map[ event.date ].push(event)
+        if (event.days) {
+          let timestamp = parseTimestamp(event.date)
+          let days = event.days
+          do {
+            timestamp = addToDate(timestamp, { day: 1 })
+            if (!map[ timestamp.date ]) {
+              map[ timestamp.date ] = []
+            }
+            map[ timestamp.date ].push(event)
+          } while (--days > 0)
+        }
+      })
+      return map
+    }
+  },
   methods: {
+    badgeClasses (event, type) {
+      const isHeader = type === 'header'
+      return {
+        [ `text-white bg-${ event.bgcolor }` ]: true,
+        'full-width': !isHeader && (!event.side || event.side === 'full'),
+        'left-side': !isHeader && event.side === 'left',
+        'right-side': !isHeader && event.side === 'right',
+        'rounded-border': true
+      }
+    },
+    badgeStyles (event, type, timeStartPos = undefined, timeDurationHeight = undefined) {
+      const s = {}
+      if (timeStartPos && timeDurationHeight) {
+        s.top = timeStartPos(event.time) + 'px'
+        s.height = timeDurationHeight(event.duration) + 'px'
+      }
+      s[ 'align-items' ] = 'flex-start'
+      return s
+    },
     onToday () {
       this.$refs.calendar.moveToToday()
     },
@@ -94,11 +184,94 @@ export default defineComponent({
     },
     onClickHeadDay (data) {
       console.log('onClickHeadDay', data)
+    } ,
+    getEvents (dt) {
+      // get all events for the specified date
+      const events = this.eventsMap[ dt ] || []
+
+      if (events.length === 1) {
+        events[ 0 ].side = 'full'
+      }
+      else if (events.length === 2) {
+        // this example does no more than 2 events per day
+        // check if the two events overlap and if so, select
+        // left or right side alignment to prevent overlap
+        const startTime = addToDate(parsed(events[ 0 ].date), { minute: parseTime(events[ 0 ].time) })
+        const endTime = addToDate(startTime, { minute: events[ 0 ].duration })
+        const startTime2 = addToDate(parsed(events[ 1 ].date), { minute: parseTime(events[ 1 ].time) })
+        const endTime2 = addToDate(startTime2, { minute: events[ 1 ].duration })
+        if (isBetweenDates(startTime2, startTime, endTime, true) || isBetweenDates(endTime2, startTime, endTime, true)) {
+          events[ 0 ].side = 'left'
+          events[ 1 ].side = 'right'
+        }
+        else {
+          events[ 0 ].side = 'full'
+          events[ 1 ].side = 'full'
+        }
+      }
+
+      return events
     },
-    
+
   },
   mounted(){
     this.employeeStore.getAttendance()
   }
 })
 </script>
+<style lang="sass" scoped>
+.my-event
+  position: absolute
+  font-size: 12px
+  justify-content: center
+  margin: 0 1px
+  text-overflow: ellipsis
+  overflow: hidden
+  cursor: pointer
+
+.title
+  position: relative
+  display: flex
+  justify-content: center
+  align-items: center
+  height: 100%
+
+.text-white
+  color: white
+
+.bg-blue
+  background: blue
+
+.bg-green
+  background: green
+
+.bg-orange
+  background: orange
+
+.bg-red
+  background: red
+
+.bg-teal
+  background: teal
+
+.bg-grey
+  background: grey
+
+.bg-purple
+  background: purple
+
+.full-width
+  left: 0
+  width: calc(100% - 2px)
+
+.left-side
+  left: 0
+  width: calc(50% - 3px)
+
+.right-side
+  left: 50%
+  width: calc(50% - 3px)
+
+.rounded-border
+  border-radius: 2px
+</style>
