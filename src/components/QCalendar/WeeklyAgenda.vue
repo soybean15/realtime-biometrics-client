93929dd871b551540381848aa0ea53dca1721637
soybean15 <marlonpadilla1593@gmail.com>
2023-09-27 21:47:49 +0800

    fixing qcalendar

--- src/components/QCalendar/WeeklyAgenda.vue
+++ src/components/QCalendar/WeeklyAgenda.vue
@@ -8,17 +8,13 @@
 
     <div class="row justify-center">
       <div style="display: flex; max-width: 800px; width: 100%; height: 400px;">
-        <q-calendar-agenda
+        <q-calendar
           ref="calendar"
           v-model="selectedDate"
           view="week"
-          :left-column-options="leftColumnOptions"
-          :right-column-options="rightColumnOptions"
-          :weekdays="[1,2,3,4,5]"
-          :day-min-height="200"
-          bordered
+          dark
           animated
-          locale="en-US"
+          bordered
           @change="onChange"
           @moved="onMoved"
           @click-date="onClickDate"
@@ -26,207 +22,48 @@
           @click-interval="onClickInterval"
           @click-head-intervals="onClickHeadIntervals"
           @click-head-day="onClickHeadDay"
+          
         >
-          <template #day="{ scope: { timestamp } }">
-            <template
-              v-for="a in getAgenda(timestamp)"
-              :key="timestamp.date + a.time"
-            >
-              <div
-                :label="a.time"
-                class="justify-start q-ma-sm shadow-5 bg-grey-6"
-                style="margin-top: 25px;"
-              >
-                <div
-                  v-if="a.avatar"
-                  class="row justify-center"
-                  style="margin-top: 30px; width: 100%;"
-                >
-                  <q-avatar style="margin-top: -50px; margin-bottom: 10px; font-size: 60px;">
-                    <img
-                      :src="a.avatar"
-                      style="border: #9e9e9e solid 5px;"
-                    >
-                  </q-avatar>
-                </div>
-                <div class="col-12 q-px-sm">
-                  <strong>{{ a.time }}</strong>
-                </div>
-                <div
-                  v-if="a.desc"
-                  class="col-12 q-px-sm"
-                  style="font-size: 10px;"
-                >
-                  {{ a.desc }}
-                </div>
-              </div>
-            </template>
-          </template>
-        </q-calendar-agenda>
+        <template #day-body="{ }">
+          test
+       
+      </template>
+
+      
+        
+      </q-calendar>
       </div>
     </div>
   </div>
-
 </template>
 
 <script>
-import { QCalendarAgenda, today } from '@quasar/quasar-ui-qcalendar/src/index.js'
+import { QCalendar ,today} from '@quasar/quasar-ui-qcalendar'
 import '@quasar/quasar-ui-qcalendar/src/QCalendarVariables.sass'
 import '@quasar/quasar-ui-qcalendar/src/QCalendarTransitions.sass'
-import '@quasar/quasar-ui-qcalendar/src/QCalendarAgenda.sass'
-
+import '@quasar/quasar-ui-qcalendar/src/QCalendarDay.sass'
+import { useEmployeeStore } from '@/store/employee'
 import { defineComponent } from 'vue'
 import NavigationBar from './NavigationBar.vue'
 
-import { useEmployeeStore } from '@/store/employee'
+
 
 
 
 export default defineComponent({
-  name: 'AgendaColumnOptions',
+  name: 'WeekDark',
   components: {
     NavigationBar,
-    QCalendarAgenda
+    QCalendar
   },
   data () {
     return {
-      selectedDate: today(),
-      employeeStore : useEmployeeStore(),
-      agenda: {
-        // value represents day of the week
-        1: [
-          {
-            time: '08:00',
-            avatar: 'https://cdn.quasar.dev/img/boy-avatar.png',
-            desc: 'Meeting with CEO'
-          },
-          {
-            time: '08:30',
-            avatar: 'https://cdn.quasar.dev/img/avatar.png',
-            desc: 'Meeting with HR'
-          },
-          {
-            time: '10:00',
-            avatar: 'https://cdn.quasar.dev/img/avatar1.jpg',
-            desc: 'Meeting with Karen'
-          }
-        ],
-        2: [
-          {
-            time: '11:30',
-            avatar: 'https://cdn.quasar.dev/img/avatar2.jpg',
-            desc: 'Meeting with Alisha'
-          },
-          {
-            time: '17:00',
-            avatar: 'https://cdn.quasar.dev/img/avatar3.jpg',
-            desc: 'Meeting with Sarah'
-          }
-        ],
-        3: [
-          {
-            time: '08:00',
-            desc: 'Stand-up SCRUM',
-            avatar: 'https://cdn.quasar.dev/img/material.png'
-          },
-          {
-            time: '09:00',
-            avatar: 'https://cdn.quasar.dev/img/boy-avatar.png'
-          },
-          {
-            time: '10:00',
-            desc: 'Sprint planning',
-            avatar: 'https://cdn.quasar.dev/img/material.png'
-          },
-          {
-            time: '13:00',
-            avatar: 'https://cdn.quasar.dev/img/avatar1.jpg'
-          }
-        ],
-        4: [
-          {
-            time: '09:00',
-            avatar: 'https://cdn.quasar.dev/img/avatar3.jpg'
-          },
-          {
-            time: '10:00',
-            avatar: 'https://cdn.quasar.dev/img/avatar2.jpg'
-          },
-          {
-            time: '13:00',
-            avatar: 'https://cdn.quasar.dev/img/material.png'
-          }
-        ],
-        5: [
-          {
-            time: '08:00',
-            avatar: 'https://cdn.quasar.dev/img/boy-avatar.png'
-          },
-          {
-            time: '09:00',
-            avatar: 'https://cdn.quasar.dev/img/avatar2.jpg'
-          },
-          {
-            time: '09:30',
-            avatar: 'https://cdn.quasar.dev/img/avatar4.jpg'
-          },
-          {
-            time: '10:00',
-            avatar: 'https://cdn.quasar.dev/img/avatar5.jpg'
-          },
-          {
-            time: '11:30',
-            avatar: 'https://cdn.quasar.dev/img/material.png'
-          },
-          {
-            time: '13:00',
-            avatar: 'https://cdn.quasar.dev/img/avatar6.jpg'
-          },
-          {
-            time: '13:30',
-            avatar: 'https://cdn.quasar.dev/img/avatar3.jpg'
-          },
-          {
-            time: '14:00',
-            avatar: 'https://cdn.quasar.dev/img/linux-avatar.png'
-          },
-          {
-            time: '14:30',
-            avatar: 'https://cdn.quasar.dev/img/avatar.png'
-          },
-          {
-            time: '15:00',
-            avatar: 'https://cdn.quasar.dev/img/boy-avatar.png'
-          },
-          {
-            time: '15:30',
-            avatar: 'https://cdn.quasar.dev/img/avatar2.jpg'
-          },
-          {
-            time: '16:00',
-            avatar: 'https://cdn.quasar.dev/img/avatar6.jpg'
-          }
-        ]
-      },
-      leftColumnOptions: [
-        {
-          id: 'over-due',
-          label: 'Over Due'
-        }
-      ],
-      rightColumnOptions: [
-        {
-          id: 'summary',
-          label: 'Summary'
-        }
-      ]
+      employeeStore:useEmployeeStore(),
+      selectedDate: today()
     }
   },
 
   methods: {
-    getAgenda (day) {
-      return this.agenda[ parseInt(day.weekday, 10) ]
-    },
     onToday () {
       this.$refs.calendar.moveToToday()
     },
@@ -236,6 +73,7 @@ export default defineComponent({
     onNext () {
       this.$refs.calendar.next()
     },
+
     onMoved (data) {
       console.log('onMoved', data)
     },
@@ -256,11 +94,11 @@ export default defineComponent({
     },
     onClickHeadDay (data) {
       console.log('onClickHeadDay', data)
-    }
+    },
+    
   },
-  onMounted:{
-     
-
+  mounted(){
+    this.employeeStore.getAttendance()
   }
 })
 </script>
