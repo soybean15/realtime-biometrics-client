e50a4bc1c0975d96bea66c19c806a7d597dd32d6
soybean15 <marlonpadilla1593@gmail.com>
2023-09-28 22:41:06 +0800

    updated attendance today

--- src/components/QCalendar/WeeklyAgenda.vue
+++ src/components/QCalendar/WeeklyAgenda.vue
@@ -9,7 +9,7 @@
     />
 
     <div class="row justify-center">
-      <div style="display: flex; max-width: 800px; width: 100%; height: 400px;">
+      <div style="display: flex; max-width: 800px; width: 100%; height: 500px;">
         <q-calendar-day
           ref="calendar"
           v-model="selectedDate"
