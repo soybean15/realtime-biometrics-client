d9a66fcb95b458e9684f3ff36a06c85aba09a307
soybean15 <marlonpadilla1593@gmail.com>
2023-09-29 21:45:49 +0800

    fix time format

--- src/views/admin/components/children/TodayAttendance.vue
+++ src/views/admin/components/children/TodayAttendance.vue
@@ -1,45 +1,57 @@
 <template>
-
-  <div class="q-px-lg q-py-md  bg-surface rounded-lg">
+  <div class="q-px-lg q-py-md bg-surface rounded-lg">
     <q-timeline color="secondary">
       <q-timeline-entry heading>
-         <div class="row justify-between">
-            <span>Today's Attendance </span>
-            <span>{{ dateTime.date }}</span>
-         </div>
-        </q-timeline-entry>
-
-        <div v-if="todayAttendance && todayAttendance.length==0">No Data Available</div>
-
-        <div v-else>
-
-            <q-timeline-entry :title="item.type" :subtitle="item.timestamp" v-for="item in todayAttendance" :key="item.id"/>
-        
-
+        <div class="row justify-between">
+          <span>Today's Attendance </span>
+          <span>{{ dateTime.date }}</span>
         </div>
-      
+      </q-timeline-entry>
 
- 
+      <div v-if="todayAttendance && todayAttendance.length == 0">
+        No Data Available
+      </div>
+
+      <div v-else>
+
+
+        <q-timeline-entry
+          :title="item.type"
+          :subtitle="format(item.timestamp, 'LT')"
+          v-for="item in todayAttendance"
+          :key="item.id"
+
+          
+        />
+      </div>
     </q-timeline>
   </div>
-
 </template>
 
   <script>
-  import { useSettingStore } from '@/store/settings';
-import { storeToRefs } from 'pinia';
+import { useSettingStore } from "@/store/settings";
+import { storeToRefs } from "pinia";
+import formatTime from "@/composables/DateTimeFormat";
 
 export default {
-    props:['todayAttendance'],
-    setup(){
-        const settingStore = useSettingStore()
+  props: ["todayAttendance"],
+  setup() {
+    const settingStore = useSettingStore();
 
-        const {dateTime}= storeToRefs(settingStore)
+    const { dateTime } = storeToRefs(settingStore);
 
+    return {
+      dateTime,
 
-        return {dateTime}
+      format: (val) => {
+        // const time = val.split(' ')
+        // return time[1]
 
+        console.log(formatTime(val,'LT').toString());
 
-    }
+        return formatTime(val,'LT').toString();
+      },
+    };
+  },
 };
 </script>
