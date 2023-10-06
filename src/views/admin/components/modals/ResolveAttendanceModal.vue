7fbaa930775880b1f1becbe4176e95cd00f0ecd4
soybean15 <marlonpadilla1593@gmail.com>
2023-10-06 17:26:55 +0800

    modified resolve modal design

--- src/views/admin/components/modals/ResolveAttendanceModal.vue
+++ src/views/admin/components/modals/ResolveAttendanceModal.vue
@@ -16,76 +16,103 @@
     </template>
 
     <template v-slot:title>
-      <div class="p-5 text-lg font-semibold">Resolve Issue</div>
+      <div class="row justify-between">
+
+        <div class="px-10 text-lg font-semibold">Resolve Issue</div>
+   
+      </div>
+    
     </template>
 
     <template v-slot:content>
+
+      <div class="px-10 text-lg ">Date: {{ formatTime(issue.date) }}</div>
       <div class="p-5">
-        <div v-for="item in issue.remarks" :key="item.key">
-          <div
-            class="row justify-between item-center"
-            v-if="item.key === 'no_time_out' || item.key === 'no_time_in'"
-          >
-            <q-chip
-              outline
-              square
-              :label="item.title"
-              :color="getChipColor(item.key)"
-            />
-            {{ setDefaultTime(item.key) }}
+        <q-list  separator>
+          <q-item clickable v-ripple>
+        <q-item-section>
 
-            <div class="row items-center">
-              <q-input
-                outlined
-                dense
-                v-model="time"
-                mask="time"
-              
-              >
-                <template v-slot:append>
-                  <q-icon name="access_time" class="cursor-pointer">
-                    <q-popup-proxy
-                      cover
-                      transition-show="scale"
-                      transition-hide="scale"
+          <div class="row justify-between">
+            <span class="font-secondary font-bold text-lg">Remarks</span>
+            <span class="w-40"></span>
+          </div>
+        </q-item-section>
+      </q-item>
+
+          <div v-for="item in issue.remarks" :key="item.key">
+            <q-item clickable v-ripple>
+              <q-item-section>
+                <div
+                  class="row justify-between item-center"
+                  v-if="item.key === 'no_time_out' || item.key === 'no_time_in'"
+                >
+                  <q-chip
+                    outline
+                    square
+                    :label="item.title"
+                    :color="getChipColor(item.key)"
+                  />
+                  {{ setDefaultTime(item.key) }}
+
+                  <div class="row items-center">
+                    <q-input outlined dense v-model="time" mask="time">
+                      <template v-slot:append>
+                        <q-icon name="access_time" class="cursor-pointer">
+                          <q-popup-proxy
+                            cover
+                            transition-show="scale"
+                            transition-hide="scale"
+                          >
+                            <q-time v-model="time">
+                              <div class="row items-center justify-end">
+                                <q-btn
+                                  v-close-popup
+                                  label="Close"
+                                  color="primary"
+                                  flat
+                                />
+                              </div>
+                            </q-time>
+                          </q-popup-proxy>
+                        </q-icon>
+                      </template>
+                    </q-input>
+
+                    <q-btn
+                      @click="
+                        attendanceStore.resolve('Time in', `${issue.date}`)
+                      "
+                      class="mx-3"
+                      dense
+                      rounded
+                      color="secondary"
+                      icon="check_circle"
                     >
-                      <q-time v-model="time">
-                        <div class="row items-center justify-end">
-                          <q-btn
-                            v-close-popup
-                            label="Close"
-                            color="primary"
-                            flat
-                          />
-                        </div>
-                      </q-time>
-                    </q-popup-proxy>
-                  </q-icon>
-                </template>
-              </q-input>
+                      <q-tooltip class="bg-indigo" :offset="[10, 10]">
+                        Resolve
+                      </q-tooltip>
+                    </q-btn>
+                  </div>
+                </div>
 
-              <q-btn @click="attendanceStore.resolve('Time in',`${issue.date}`)" class="mx-3" dense round color="secondary" icon="check_circle">
+                <div v-else>
+                  <q-chip
+                    outline
+                    square
+                    :label="item.title"
+                    :color="getChipColor(item.key)"
+                  />
+                </div>
+              </q-item-section>
+            </q-item>
 
-                <q-tooltip  class="bg-indigo" :offset="[10, 10]">
-                Resolve
-                </q-tooltip>
-              </q-btn>
-            </div>
+            <q-separator/>
           </div>
-
-          <div v-else>
-            <q-chip
-              outline
-              square
-              :label="item.title"
-              :color="getChipColor(item.key)"
-            />
-          </div>
-        </div>
+        </q-list>
       </div>
     </template>
   </PersistentDialog>
-  {{ issue }}
+
 </template>
 
 <script>
@@ -95,16 +122,19 @@ import { useSettingStore } from "@/store/settings";
 import { storeToRefs } from "pinia";
 import { ref } from "vue";
 import { useAttendanceStore } from "@/store/attendance";
+import formatTime from "@/composables/DateTimeFormat";
+
 export default {
   components: { PersistentDialog },
   props: ["issue"],
   setup() {
-    const settingStore = useSettingStore()
-    const  attendanceStore = useAttendanceStore()
+    const settingStore = useSettingStore();
+    const attendanceStore = useAttendanceStore();
     const time = ref(null);
     const { settings } = storeToRefs(settingStore);
 
     return {
+      formatTime,
       getChipColor,
       time,
       attendanceStore,
