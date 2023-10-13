e9068bc6b6ab549e3f0dc2d4ed831346a945171a
soybean15 <marlonpadilla1593@gmail.com>
2023-10-13 22:43:25 +0800

    added summary view

---
+++ src/views/admin/components/children/AttendanceSummary.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+
+        <div class="row">
+            <SummaryCard
+                :title="'Attendance'"
+                titleStyle="font-bold text-lg text-green"
+                :value="'13/15'"
+            />
+
+            <SummaryCard
+                :title="'Lates'"
+                titleStyle="font-bold text-lg text-orange"
+                :value="'13/15'"
+            />
+        </div>
+
+        
+    </div>
+
+
+</template>
+
+<script>
+import SummaryCard from './SummaryCard.vue'
+
+export default {
+    components:{SummaryCard}
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/admin/components/children/SummaryCard.vue
@@ -0,0 +1,31 @@
+<template>
+  
+  <div class="bg-onSurface rounded-md shadow-lg p-5">
+    <div :class="titleStyle">
+        {{title}}
+
+    </div>
+
+    <div class="row justify-center">
+        {{value}}
+
+    </div>
+
+    
+
+     
+
+  </div>
+</template>
+
+<script>
+export default {
+
+    props:['title','titleStyle','value']
+
+}
+</script>
+
+<style>
+
+</style>
