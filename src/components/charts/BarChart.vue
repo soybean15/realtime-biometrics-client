ab675e42588aeabd585e82b386c2eae46e26ac35
soybean15 <marlonpadilla1593@gmail.com>
2023-10-28 21:31:35 +0800

    start adding chart

---
+++ src/components/charts/BarChart.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <apexchart
+      width="500"
+      type="bar"
+      :options="chartOptions"
+      :series="series"
+    ></apexchart>
+  </div>
+</template>
+
+<script>
+// you can import in a particular component and register the component like below
+import VueApexCharts from "vue3-apexcharts";
+export default {
+  components: {
+    apexchart: VueApexCharts,
+  },
+
+  setup(){
+    return {
+      chartOptions: {
+        chart: {
+          id: "vuechart-example",
+        },
+        xaxis: {
+          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+        },
+      },
+      series: [
+        {
+          name: "series-1",
+          data: [30, 40, 35, 50, 49, 60, 70, 91],
+        },
+      ],
+    };
+  }
+};
+</script>
+
+<style>
+
+</style>
--- src/views/admin/DashboardView.vue
+++ src/views/admin/DashboardView.vue
@@ -1,13 +1,46 @@
 <template>
-  Dashboard
+  <div class="row">
+    <div class="col-8 column p-5">
+      <div>
+        <div class="text-4xl font-thin">Dashboard</div>
+
+        <div class="text-md font-secondary">Welcome back, {{ user.name }}</div>
+      </div>
+    </div>
+
+    <bar-chart/>
+
+    <div class="col-4"></div>
+  </div>
 </template>
 
 <script>
-export default {
+import { useDashboardStore } from "@/store/dashboard";
+import { onMounted } from "vue";
+import { storeToRefs } from "pinia";
 
-}
+import BarChart from '@/components/charts/BarChart.vue';
+export default {
+  components:{
+    BarChart,
+  },
+  setup() {
+    const dashboardStore = useDashboardStore();
+
+    const { dashboard, user, summary } = storeToRefs(dashboardStore);
+
+    onMounted(() => {
+      dashboardStore.index();
+    });
+
+    return {
+      dashboard,
+      user,
+      summary,
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>
