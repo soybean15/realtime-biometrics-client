f28cffabf03e97db683079381ef8a9b134f36c09
soybean15 <marlonpadilla1593@gmail.com>
2023-10-28 23:00:35 +0800

    added dashboard

--- src/components/charts/BarChart.vue
+++ src/components/charts/BarChart.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <apexchart
-      width="500"
+      width="100%"
+      height="400px"
       type="bar"
       :options="chartOptions"
       :series="series"
@@ -17,27 +18,73 @@ export default {
     apexchart: VueApexCharts,
   },
 
-  setup(){
+  props: ["seriesProps", "categoriesProps"],
+  setup(props) {
     return {
+      series: props.seriesProps,
       chartOptions: {
+        colors: ["#168216", "#b05713"],
         chart: {
-          id: "vuechart-example",
+          type: "bar",
+          height: 350,
+          stacked: true,
+          toolbar: {
+            show: true,
+          },
+          zoom: {
+            enabled: true,
+          },
+        },
+        responsive: [
+          {
+            breakpoint: 480,
+            options: {
+              legend: {
+                position: "bottom",
+                offsetX: -10,
+                offsetY: 0,
+              },
+            },
+          },
+        ],
+        plotOptions: {
+          bar: {
+            horizontal: false,
+            borderRadius: 10,
+            dataLabels: {
+              total: {
+                enabled: true,
+                style: {
+                  fontSize: "13px",
+                  fontWeight: 900,
+                },
+              },
+            },
+          },
         },
         xaxis: {
-          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+          type: "datetime",
+          categories: props.categoriesProps,
+        },
+        yaxis: {
+            max:100
+          
+        },
+        legend: {
+          position: "right",
+          offsetY: 40,
+        },
+        fill: {
+          opacity: 1,
         },
       },
-      series: [
-        {
-          name: "series-1",
-          data: [30, 40, 35, 50, 49, 60, 70, 91],
-        },
-      ],
     };
-  }
+  },
 };
 </script>
 
 <style>
-
+.apexcharts-tooltip {
+  color: #1b1a1a;
+}
 </style>
--- src/views/admin/DashboardView.vue
+++ src/views/admin/DashboardView.vue
@@ -6,37 +6,115 @@
 
         <div class="text-md font-secondary">Welcome back, {{ user.name }}</div>
       </div>
+      <div class="p-1 bg-surface my-2 shadow-md rounded-md">
+        <bar-chart
+        :seriesProps="series.series"
+        :categoriesProps="series.categories"
+        v-if="series"
+      />
+
+     
+      </div>
+      <div class="bg-surface row rounded-md my-1 p-1">
+        <StatView
+        class="mx-0.5"
+           :backgroundColor="'bg-onSurface'"
+          :title='{
+            value:"Total Users",
+            properties:"text-green"
+          }'
+          :value="dashboard.user_count"
+          
+        v-if="dashboard"/>
+        <StatView
+          class="mx-0.5"
+          :backgroundColor="'bg-onSurface'"
+          :title='{
+            value:"Total Employees",
+            properties:"text-green"
+          }'
+          :value="dashboard.employee_count"
+          
+        v-if="dashboard"/>
+
+      </div>
+     
     </div>
 
-    <bar-chart/>
+    <div class="col-4">
 
-    <div class="col-4"></div>
+     
+   
+    </div>
   </div>
+ 
 </template>
 
 <script>
 import { useDashboardStore } from "@/store/dashboard";
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import { storeToRefs } from "pinia";
 
-import BarChart from '@/components/charts/BarChart.vue';
+
+import BarChart from "@/components/charts/BarChart.vue";
+import StatView from '@/components/StatView.vue';
+
+const generateChartData = (data) => {
+  const latesData = [];
+  const attendendData = [];
+
+  const categories = [];
+
+  for (let date in data) {
+    console.log(data[date].month);
+
+    const lates = data[date].total_lates;
+    const attendance = data[date].total_attendance;
+
+    latesData.push((lates == 0 ? 0 : (lates / attendance) * 100).toFixed(2));
+    attendendData.push((100-data[date].absentee_rate ).toFixed(2));
+    categories.push(date);
+  }
+
+  return {
+    series: [
+    
+      {
+        name: "Attendance",
+        data: attendendData,
+      },
+      {
+        name: "Lates",
+        data: latesData,
+      },
+    ],
+    categories:categories
+  };
+};
 export default {
-  components:{
+  components: {
     BarChart,
+    StatView
   },
   setup() {
     const dashboardStore = useDashboardStore();
 
+    const series = ref(null);
     const { dashboard, user, summary } = storeToRefs(dashboardStore);
 
-    onMounted(() => {
-      dashboardStore.index();
+    onMounted(async () => {
+      await dashboardStore.index();
+
+      console.log(summary.value.data);
+      series.value = generateChartData(summary.value.data);
     });
 
     return {
       dashboard,
       user,
+      series,
       summary,
+      
     };
   },
 };
