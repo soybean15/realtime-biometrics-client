11954e4b53e0835ecc96fb4fffeb897cc82e74d5
soybean15 <marlonpadilla1593@gmail.com>
2023-10-30 19:54:33 +0800

    finished dashboard

--- src/components/StatView.vue
+++ src/components/StatView.vue
@@ -1,50 +1,45 @@
 <template>
-    <div class="p-4" :class="backgroundColor" style="min-width: 200px;">
-        <div class="text-lg " :class="title.properties">{{title.value}}</div>
-        <div class="text-3xl row font-semibold items-center">
-            {{value}}
-            <div class="row items-center" v-if="caption"> 
-                <div class="font-secondary text-xs px-1">{{caption.separator}}</div>
-                <div class="text-sm ">{{caption.value}} </div>
-             </div>
-        </div>
-
-        <div class="row items-center">
-            <q-icon size="2em" :color="helper.isPositive? 'green':'red'" :name="helper.isUp ? 'arrow_drop_up' : 'arrow_drop_down' " v-if="helper"/>
-
-           <span class="font-secondary">
-            {{helperText}}
-
-           </span>
-           <slot name="helper">
-           </slot>
-
-       
-
-        </div>
-
-
-
-
+  <div class="p-4" :class="backgroundColor" style="min-width: 200px">
+    <div class="text-lg" :class="title.properties">{{ title.value }}</div>
+    <div class="text-3xl row font-semibold items-center">
+      {{ value }}
+      <div class="row items-center" v-if="caption">
+        <div class="font-secondary text-xs px-1">{{ caption.separator }}</div>
+        <div class="text-sm font-secondary">{{ caption.value }}</div>
+      </div>
     </div>
 
+    <div class="column text-xs font-secondary">
+      <div class="row items-center">
+        {{ helperText }}
+      </div>
+
+      <div class="row items-center">
+        <q-icon
+          size="2em"
+          :color="helper.isPositive ? 'green' : 'red'"
+          :name="helper.isUp ? 'arrow_drop_up' : 'arrow_drop_down'"
+          v-if="helper"
+        />
+
+        <slot name="helper"> </slot>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    props:[
-        'backgroundColor',
-        'helper',
-        'title',
-        'value',
-        'helperText',
-        'caption'
-        
-    ]
-
-}
+  props: [
+    "backgroundColor",
+    "helper",
+    "title",
+    "value",
+    "helperText",
+    "caption",
+  ],
+};
 </script>
 
 <style>
-
 </style>
--- src/views/admin/DashboardView.vue
+++ src/views/admin/DashboardView.vue
@@ -8,29 +8,105 @@
 
     <div class="row">
       <div class="col-7 column">
+        <div class="row rounded-md my-1 p-1">
+          <StatView
+            class="m-0.5"
+            :backgroundColor="'bg-surface'"
+            :title="{
+              value: 'Total Users',
+              properties: 'text-green',
+            }"
+            :value="dashboard.user_count"
+            v-if="dashboard"
+          />
+          <StatView
+            class="m-0.5"
+            :backgroundColor="'bg-surface'"
+            :title="{
+              value: 'Total Employees',
+              properties: 'text-green',
+            }"
+            :value="dashboard.employee_count"
+            v-if="dashboard"
+          />
+          <StatView
+            class="m-0.5"
+            :backgroundColor="'bg-surface'"
+            :title="{
+              value: 'Total working days',
+              properties: 'text-green',
+            }"
+            :value="currentAttendance ? currentAttendance.total_attendance : 0"
+            :caption="{
+              value: `${currentAttendance.total_lates} total lates`,
+              separator: '',
+            }"
+            :helper="{
+              isUp:
+                currentAttendance.total_lates > previousAttendance.total_lates,
+              isPositive:
+                currentAttendance.total_lates < previousAttendance.total_lates,
+            }"
+            v-if="dashboard && currentAttendance"
+          >
+            <template v-slot:helper>
+              <div>
+                <!-- {{`${(((currentAttendance.total_lates-previousAttendance.total_lates)/currentAttendance.total_lates)*100).toFixed(0)}% higher compare last month`}} -->
+                {{
+                  computeRate(
+                    currentAttendance.total_lates,
+                    previousAttendance.total_lates,
+                    (result, isHigher) => {
+                      result = (result / currentAttendance.total_lates) * 100;
 
-        <div class=" row rounded-md my-1 p-1">
-              <StatView
-                class="mx-0.5"
-                :backgroundColor="'bg-surface'"
-                :title="{
-                  value: 'Total Users',
-                  properties: 'text-green',
-                }"
-                :value="dashboard.user_count"
-                v-if="dashboard"
-              />
-              <StatView
-                class="mx-0.5"
-                :backgroundColor="'bg-surface'"
-                :title="{
-                  value: 'Total Employees',
-                  properties: 'text-green',
-                }"
-                :value="dashboard.employee_count"
-                v-if="dashboard"
-              />
-            </div>
+                      if (isHigher) {
+                        return `${result.toFixed(0)}% higher than last month`;
+                      } else {
+                        return `${result.toFixed(0)}% lower than last month`;
+                      }
+                    }
+                  )
+                }}
+              </div>
+            </template>
+          </StatView>
+
+          <StatView
+            class="m-0.5"
+            :backgroundColor="'bg-surface'"
+            :title="{
+              value: 'Absentee Rate',
+              properties: 'text-green',
+            }"
+            :value="`${currentAttendance.absentee_rate.toFixed(2)}%`"
+            :helper="{
+              isUp:
+                currentAttendance.total_lates > previousAttendance.total_lates,
+              isPositive:
+                currentAttendance.total_lates < previousAttendance.total_lates,
+            }"
+            v-if="dashboard && currentAttendance"
+          >
+            <template v-slot:helper>
+              <div>
+                {{
+                  computeRate(
+                    currentAttendance.total_lates,
+                    previousAttendance.total_lates,
+                    (result, isHigher) => {
+        
+                      if (isHigher) {
+                        return `${result.toFixed(0)}% higher than last month`;
+                      } else {
+                        return `${result.toFixed(0)}% lower than last month`;
+                      }
+                    }
+                  )
+                }}
+              </div>
+            </template>
+          </StatView>
+        </div>
         <div class="p-3 bg-surface my-2 shadow-md rounded-md">
           <bar-chart
             :title="'Attendance Rate'"
@@ -38,20 +114,17 @@
             :categoriesProps="series.categories"
             v-if="series"
           />
-
-      
         </div>
-
-       
       </div>
 
       <div class="col-5 p-2 rounded-md">
         <div class="bg-surface p-4">
-          <PieChart :title="'Attendance Rate by Department'"
+          <PieChart
+            :title="'Attendance Rate by Department'"
             :options="pieChartOption"
-            
             @onChange="onChange"
-          v-if="pieChartOption"/>
+            v-if="pieChartOption"
+          />
         </div>
       </div>
     </div>
@@ -67,9 +140,9 @@ import { storeToRefs } from "pinia";
 import BarChart from "@/components/charts/BarChart.vue";
 import StatView from "@/components/StatView.vue";
 import PieChart from "@/components/charts/PieChart.vue";
-import generateBarChartData from '@/composables/barChart'
-import generatePieChartData from '@/composables/pieChart'
-import { date } from 'quasar'
+import generateBarChartData from "@/composables/barChart";
+import generatePieChartData from "@/composables/pieChart";
+import { date } from "quasar";
 
 export default {
   components: {
@@ -81,27 +154,32 @@ export default {
     const dashboardStore = useDashboardStore();
 
     const series = ref(null);
-    const { dashboard, user, summary,pieChartData } = storeToRefs(dashboardStore);
+    const currentAttendance = ref(null);
+    const previousAttendance = ref(null);
+    const { dashboard, user, summary, pieChartData } =
+      storeToRefs(dashboardStore);
 
-    const pieChartOption = ref(null)
+    const pieChartOption = ref(null);
 
     onMounted(async () => {
       await dashboardStore.index();
 
       series.value = generateBarChartData(summary.value.data);
 
-  
-      pieChartOption.value = Object.keys(summary.value.data).map(
-        (item)=>{
+      pieChartOption.value = Object.keys(summary.value.data).map((item) => {
+        return {
+          label: date.formatDate(item, "MMM YYYY"),
+          value: item,
+        };
+      });
 
-          return {
-            label:date.formatDate(item, 'MMM YYYY'),
-            value: item
-          }
-        }
-      )
-
-      pieChartData.value = generatePieChartData(summary.value.data [pieChartOption.value[0].value])
+      pieChartData.value = generatePieChartData(
+        summary.value.data[pieChartOption.value[0].value]
+      );
+      currentAttendance.value =
+        summary.value.data[pieChartOption.value[0].value];
+      previousAttendance.value =
+        summary.value.data[pieChartOption.value[1].value];
     });
 
     return {
@@ -109,12 +187,26 @@ export default {
       user,
       series,
       summary,
-      pieChartOption ,
-      onChange:(value)=>{
-      
-        pieChartData.value =  generatePieChartData(summary.value.data [value])
-
-      }
+      pieChartOption,
+      currentAttendance,
+      previousAttendance,
+      onChange: (value) => {
+        pieChartData.value = generatePieChartData(summary.value.data[value]);
+      },
+      computeRate: (current, prev, callback) => {
+        let result = "";
+        let isHigher = true;
+        if (current >= prev) {
+          result = current - prev;
+          return callback(result, isHigher);
+        } else if (current < prev) {
+          isHigher = false;
+          result = prev - current;
+          return callback(result, isHigher);
+        } else {
+          return "No changes";
+        }
+      },
     };
   },
 };
